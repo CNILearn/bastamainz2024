0066181cecdd01acf85c87da3b4405f4d264feb8
Christian Nagel <christian@christiannagel.com>
2024-09-16 21:40:43 +0200

    minimal api

---
+++ csharp/00a-MinimalAPI/MinimalAPI/Data/BooksContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace MinimalAPI;
+
+public class BooksContext(DbContextOptions<BooksContext> options) : DbContext(options)
+{
+    public DbSet<MinimalAPI.Book> Book => Set<Book>();
+}
